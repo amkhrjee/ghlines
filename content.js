889f645e5e325c35dbef664a2b9aa18950217e44
Aniruddha Mukherjee <aniruddha.mukherji@outlook.com>
2024-05-07 02:44:39 +0530

    mostly works

--- content.js
+++ content.js
@@ -18,23 +18,52 @@ headerRow.insertBefore(linesTh, headerRow.children[2]);
 // Sets the line numbers for files
 const treeRows = tBody.children;
 
-commitMessageStyle = treeRows[1].children[1].children[0].style;
-
 setTimeout(() => {
   if (treeRows.length) {
     let isFirstRow = true;
     let count = 1;
     for (row of treeRows) {
-      console.log(`Row ${count++}`);
       if (!isFirstRow) {
         const linesCountTd = document.createElement("td");
         const linesCountSpan = document.createElement("span");
         linesCountTd.appendChild(linesCountSpan);
-        linesCountSpan.textContent = "123";
+        linesCountSpan.textContent = "Loading";
         linesCountTd.colSpan = "1";
-        // linesCountTd.style = row.style;
         linesCountSpan.style.color = "var(--fgColor-muted)";
         row.insertBefore(linesCountTd, row.children[2]);
+        const fileName =
+          row.children[1]?.children[0]?.children[1]?.children[0]?.children[0]
+            .children[0].title;
+        let lineCount = "Loading";
+        if (fileName && fileName.includes(".")) {
+          console.log(fileName);
+          //  Fetch the line count
+          fetch(
+            `https://raw.githubusercontent.com/${location.href
+              .slice(18)
+              .replace("/tree/", "/")}/${fileName}`
+          )
+            .then((response) => response.body)
+            .then(async (readableStream) => {
+              const reader = readableStream.getReader();
+              let done = false;
+              while (!done) {
+                const { done: chunkDone, value } = await reader.read();
+                done = chunkDone;
+                if (value) {
+                  lineCount = value.filter((x) => x === 10).length;
+                  linesCountSpan.textContent = lineCount;
+
+                  console.log(lineCount);
+                }
+              }
+            })
+            .catch((e) => {
+              console.error(e);
+            });
+        } else {
+          linesCountSpan.textContent = "";
+        }
       } else {
         isFirstRow = false;
       }
